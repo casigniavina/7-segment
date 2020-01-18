d65d6571c96f0b17a4d8551346fde69fd9919e72
casigniavina <casigniavina@gmail.com>
2020-01-18 16:51:46 +0800

    7segment

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -105,7 +105,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.White
+        Me.BackColor = System.Drawing.Color.Silver
         Me.ClientSize = New System.Drawing.Size(464, 451)
         Me.Controls.Add(Me.G)
         Me.Controls.Add(Me.F)
